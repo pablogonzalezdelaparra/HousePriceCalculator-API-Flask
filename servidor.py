aec86cb1323c3e3296c63875e9f7b30cdf01d492
Pablo González de la Parra <pablo.gonzalezdlp@gmail.com>
2023-09-02 21:52:35 -0600

    Crear modelo y pruebas de rutas

--- servidor.py
+++ servidor.py
@@ -10,6 +10,13 @@ dt=load('dt1.joblib')
 
 servidorWeb = Flask(__name__)
 
+# Ruta para entrenar el modelo
+@servidorWeb.route("/entrenar", methods=['GET'])
+def entrenar():
+    # Llamar wine_quality.py para entrenar el modelo
+    os.system('python wine_quality.py')
+    return jsonify({'resultado': 'ok'})
+
 @servidorWeb.route("/holamundo", methods=['GET'])
 def holaMundo():
     return render_template('pagina1.html')
@@ -17,9 +24,14 @@ def holaMundo():
 # Envío de datos a través de JSON
 @servidorWeb.route("/modelo", methods=['POST'])
 def modelo():
-    contenido = request.json
-    print(contenido)
-    return jsonify({'resultado': 'ok'})
+    # Recibir los datos de la petición
+    datos = request.get_json(force=True)
+    # Convertir los datos en un array
+    datos_array = np.array(datos['datos'])
+    # Predecir el valor de la calidad del vino
+    prediccion = dt.predict(datos_array)
+    # Retornar la predicción en formato JSON
+    return jsonify({'prediccion': prediccion.tolist()})
 
 if __name__ == '__main__':
     servidorWeb.run(debug=False, host='0.0.0.0', port='8080')
