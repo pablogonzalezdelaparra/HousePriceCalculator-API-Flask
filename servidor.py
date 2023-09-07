cec45e15b34dd0cf79427b5e118b4d1eba75162d
Pablo González de la Parra <pablo.gonzalezdlp@gmail.com>
2023-09-06 19:08:00 -0600

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -27,11 +27,16 @@ def modelo():
     # Recibir los datos de la petición
     datos = request.get_json(force=True)
     # Convertir los datos en un array
-    datos_array = np.array(datos['datos'])
+    datos_array = np.array([
+        0.88, 0, 2.6, 0.098, 25, 67, 0.9968,
+        datos['ph'],
+        datos['sulfato'],
+        datos['alcohol'],
+    ])
     # Predecir el valor de la calidad del vino
-    prediccion = dt.predict(datos_array)
+    prediccion = dt.predict(datos_array.reshape(1, -1))
     # Retornar la predicción en formato JSON
-    return jsonify({'prediccion': prediccion.tolist()})
+    return jsonify({'prediccion': str(prediccion[0])})
 
 if __name__ == '__main__':
     servidorWeb.run(debug=False, host='0.0.0.0', port='8080')
