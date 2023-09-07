a3ef7977664729508cc4a26d838b7c8a4e398fa7
Pablo González de la Parra <pablo.gonzalezdlp@gmail.com>
2023-09-06 19:09:20 -0600

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -17,9 +17,20 @@ def holaMundo():
 # Envío de datos a través de JSON
 @servidorWeb.route("/modelo", methods=['POST'])
 def modelo():
-    contenido = request.json
-    print(contenido)
-    return jsonify({'resultado': 'ok'})
+    # Recibir los datos de la petición
+    datos = request.get_json(force=True)
+    # Convertir los datos en un array
+    datos_array = np.array([
+        0.88, 0, 2.6, 0.098, 25, 67, 0.9968,
+        datos['ph'],
+        datos['sulfato'],
+        datos['alcohol'],
+    ])
+    # Predecir el valor de la calidad del vino
+    prediccion = dt.predict(datos_array.reshape(1, -1))
+
+    # Retornar la predicción en formato JSON
+    return jsonify({'prediccion': str(prediccion[0])})
 
 if __name__ == '__main__':
     servidorWeb.run(debug=False, host='0.0.0.0', port='8080')
