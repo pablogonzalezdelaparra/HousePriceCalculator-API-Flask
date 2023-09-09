00ec653b0999d9cd638e4921bcad929872d59e2d
Pablo González de la Parra <pablo.gonzalezdlp@gmail.com>
2023-09-09 11:11:50 -0600

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -33,5 +33,24 @@ def modelo():
     # Retornar la predicción en formato JSON
     return jsonify({'prediccion': str(prediccion[0])})
 
+# Envío de datos a través de JSON
+@servidorWeb.route("/modeloForm", methods=['POST'])
+def modeloForm():
+    # Recibir los datos de la petición
+    datos = request.form
+    print(datos)
+    # Convertir los datos en un array
+    datos_array = np.array([
+        0.88, 0, 2.6, 0.098, 25, 67, 0.9968, 1, 0.4,
+        datos['ph'],
+        datos['sulfato'],
+        datos['alcohol'],
+    ])
+    # Predecir el valor de la calidad del vino
+    prediccion = dt.predict(datos_array.reshape(1, -1))
+
+    # Retornar la predicción en formato JSON
+    return jsonify({'prediccion': str(prediccion[0])})
+
 if __name__ == '__main__':
     servidorWeb.run(debug=False, host='0.0.0.0', port='8080')
